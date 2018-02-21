91e2210d53d2f5ee0e022763c05aa695e3bfd31a
Frederikam <frogkr@gmail.com>
2018-02-21 14:20:06 +0100

    WIP speech

--- src/main/java/com/frederikam/robotchess/Launcher.java
+++ src/main/java/com/frederikam/robotchess/Launcher.java
@@ -2,9 +2,10 @@ package com.frederikam.robotchess;
 
 public class Launcher {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         switch (args.length != 0 ? args[0] : "default") {
             case "voice-test":
+                SpeechService.streamingRecognizeFile("test.wav");
                 break;
         }
     }
