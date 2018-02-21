d47f93aab83c7434a613514fd4f3f41eb9a2251f
Frederikam <frogkr@gmail.com>
2018-02-21 11:08:30 +0100

    Main class

---
+++ src/main/java/com/frederikam/robotchess/Launcher.java
@@ -0,0 +1,12 @@
+package com.frederikam.robotchess;
+
+public class Launcher {
+
+    public static void main(String[] args) {
+        switch (args.length != 0 ? args[0] : "default") {
+            case "voice-test":
+                break;
+        }
+    }
+
+}
