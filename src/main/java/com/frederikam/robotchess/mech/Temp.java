91a65c1a31982b81da3dbd717434db759d601403
Frederikam <frogkr@gmail.com>
2018-03-23 13:33:17 +0100

    Add main class for testing

---
+++ src/main/java/com/frederikam/robotchess/mech/Temp.java
@@ -0,0 +1,30 @@
+package com.frederikam.robotchess.mech;
+
+import com.pi4j.io.gpio.RaspiPin;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Temp {
+
+    private static final Logger log = LoggerFactory.getLogger(Temp.class);
+
+    public static void main(String[] args) throws InterruptedException {
+        StepperMotor stepperX = new StepperMotor(RaspiPin.GPIO_00, RaspiPin.GPIO_01, RaspiPin.GPIO_02, RaspiPin.GPIO_03, RaspiPin.GPIO_09);
+        StepperMotor stepperY = new StepperMotor(RaspiPin.GPIO_04, RaspiPin.GPIO_05, RaspiPin.GPIO_06, RaspiPin.GPIO_07, RaspiPin.GPIO_10);
+
+        boolean b = false;
+        long time = System.currentTimeMillis();
+        while (true) {
+            b = !b;
+            log.info(stepperX.getPosition() + "");
+            stepperX.step(b ? 400 : -400, 2);
+            log.info("Difference: {}", System.currentTimeMillis() - time);
+            time = System.currentTimeMillis();
+        }
+
+        /*ExecutorService exec = Executors.newFixedThreadPool(2);
+        exec.submit(() -> stepperX.step(1600, 2));
+        exec.submit(() -> stepperY.step(-1600, 2));
+        exec.awaitTermination(1, TimeUnit.DAYS);*/
+    }
+}
