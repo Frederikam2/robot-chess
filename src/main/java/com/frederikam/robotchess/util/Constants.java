1dda8faadf1c1fb51b38c1086ed05baa8f27d414
Frederikam <frogkr@gmail.com>
2018-04-18 10:16:44 +0200

    Mess with constants

--- src/main/java/com/frederikam/robotchess/util/Constants.java
+++ src/main/java/com/frederikam/robotchess/util/Constants.java
@@ -4,9 +4,10 @@ import com.frederikam.robotchess.mech.StepPosition;
 
 public class Constants {
 
-    public static final StepPosition BOARD_BOTTOM_LEFT = new StepPosition(0,0);
     public static final StepPosition BOARD_TOP_RIGHT = new StepPosition(10000, 6280);
     public static final double TILE_WIDTH = BOARD_TOP_RIGHT.y / 8;
+    public static final StepPosition BOARD_BOTTOM_LEFT = new StepPosition(TILE_WIDTH * 2, 0);
+
     public static final double HALF_TILE_WIDTH = TILE_WIDTH/2;
     public static final double PIECE_DIAMETER = TILE_WIDTH/2;
 
