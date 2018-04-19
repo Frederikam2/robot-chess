082bca86cc19acbead1e7da04d5eefd5bee19e9f
Frederikam <frogkr@gmail.com>
2018-04-19 10:49:31 +0200

    Calibration

--- src/main/java/com/frederikam/robotchess/util/Constants.java
+++ src/main/java/com/frederikam/robotchess/util/Constants.java
@@ -4,13 +4,14 @@ import com.frederikam.robotchess.mech.StepPosition;
 
 public class Constants {
 
-    public static final StepPosition BOARD_TOP_RIGHT = new StepPosition(10000, 6280);
-    public static final double TILE_WIDTH = BOARD_TOP_RIGHT.y / 7;
-    public static final StepPosition BOARD_BOTTOM_LEFT = new StepPosition(
-            TILE_WIDTH * 2 // Offset by two tiles for the graveyard
-                    // Center on the X axis
-                    + (BOARD_TOP_RIGHT.x - TILE_WIDTH * 11) / 2 // "TILE_WIDTH * 11" is the effectively used width
-            , 0);
+    private static final double LEFT_X = 2275;
+    private static final double LEFT_Y = 100;
+    private static final double RIGHT_X = 8190;
+    private static final double RIGHT_Y = 6100; // force 8275.0 6100
+
+    public static final double TILE_WIDTH = (RIGHT_Y - LEFT_Y) / 7;
+    public static final StepPosition BOARD_BOTTOM_LEFT = new StepPosition(LEFT_X, LEFT_Y);
+    public static final StepPosition BOARD_TOP_RIGHT = new StepPosition(LEFT_X + TILE_WIDTH * 7, RIGHT_Y);
 
     public static final double HALF_TILE_WIDTH = TILE_WIDTH / 2;
     public static final double PIECE_DIAMETER = TILE_WIDTH / 2;
